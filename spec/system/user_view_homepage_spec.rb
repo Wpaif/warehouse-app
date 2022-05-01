f8ab1b1fc71ca5f6b9ad4c6db2d6bee8d2f65217
Wilian Ferreira <wpaife@gmail.com>
2022-04-30 23:58:29 -0300

    Usuário vê galpões na tela inicial

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -1,13 +1,43 @@
 require 'rails_helper'
 
-describe "O usuario visita a tela inicial" do
-  it "e vê o nome do app" do
+describe 'O usuario visita a tela inicial' do
+  it 'e vê o nome do app' do
     # Arrange
 
-    # Act 
-    visit '/'
+    # Act
+    visit('/')
+
     # Assert
     expect(page).to have_content('Galpões & Estoque')
   end
-end
 
+  it 'e vê os galpões cadastrados' do
+    # Arrage
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+    Warehouse.create(name: 'Maceió', code: 'MCZ', city: 'Maceió', area: 50_000)
+
+    # Act
+    visit('/')
+
+    # Assert
+    expect(page).not_to have_content('Não existem galpões cadastrados')
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000 m²')
+
+    expect(page).to have_content('Maceió')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceió')
+    expect(page).to have_content('50000 m²')
+  end
+
+  it 'e não tem galpões cadastrados' do
+    # Arrange
+
+    # Act
+    visit('/')
+    # Assert
+    expect(page).to have_content('Não existem galpões cadastrados')
+  end
+end
