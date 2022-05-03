0f69d530cd5df07a2d065f55e51d907e8f834c5e
Wilian Ferreira <wpaife@gmail.com>
2022-05-03 02:55:11 -0300

    Usuário vê detalhes do galpão

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 describe 'O usuario visita a tela inicial' do
   it 'e vê o nome do app' do
-    # Arrange
-
     # Act
-    visit('/')
+    visit root_path
 
     # Assert
-    expect(page).to have_content('Galpões & Estoque')
+    expect(page).to have_content 'Galpões & Estoque'
   end
 
   it 'e vê os galpões cadastrados' do
@@ -17,27 +15,25 @@ describe 'O usuario visita a tela inicial' do
     Warehouse.create(name: 'Maceió', code: 'MCZ', city: 'Maceió', area: 50_000)
 
     # Act
-    visit('/')
+    visit root_path
 
     # Assert
-    expect(page).not_to have_content('Não existem galpões cadastrados')
-    expect(page).to have_content('Rio')
-    expect(page).to have_content('Código: SDU')
-    expect(page).to have_content('Cidade: Rio de Janeiro')
-    expect(page).to have_content('60000 m²')
+    expect(page).not_to have_content 'Não existem galpões cadastrados'
+    expect(page).to have_content 'Rio'
+    expect(page).to have_content 'Código: SDU' 
+    expect(page).to have_content 'Cidade: Rio de Janeiro' 
+    expect(page).to have_content '60000 m²'
 
-    expect(page).to have_content('Maceió')
-    expect(page).to have_content('Código: MCZ')
-    expect(page).to have_content('Cidade: Maceió')
-    expect(page).to have_content('50000 m²')
+    expect(page).to have_content 'Maceió'
+    expect(page).to have_content 'Código: MCZ'
+    expect(page).to have_content 'Cidade: Maceió'
+    expect(page).to have_content '50000 m²'
   end
 
   it 'e não tem galpões cadastrados' do
-    # Arrange
-
     # Act
-    visit('/')
+    visit root_path
     # Assert
-    expect(page).to have_content('Não existem galpões cadastrados')
+    expect(page).to have_content 'Não existem galpões cadastrados'
   end
 end
